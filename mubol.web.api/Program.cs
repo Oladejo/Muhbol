c604982521b4f6c98390df0c8d57a27d45bbb657
Oladejo <bab.oladejo@student.oauife.edu.ng>
2018-09-02 12:08:50 +0100

    set up muhbol solution

--- mubol.web.api/Program.cs
+++ mubol.web.api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace muhbol.entities
+namespace mubol.web.api
 {
     public class Program
     {
